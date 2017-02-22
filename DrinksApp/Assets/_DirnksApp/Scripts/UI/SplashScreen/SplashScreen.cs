103478de39f76cabcdc81321b80d9328a8976462
muthusamy <muthusamykb@gmail.com>
2017-02-23 00:06:33 +0530

    - Twitter implemented and Know ur drink (Brand detail page created) & Android version issue fixed

--- DrinksApp/Assets/_DirnksApp/Scripts/UI/SplashScreen/SplashScreen.cs
+++ DrinksApp/Assets/_DirnksApp/Scripts/UI/SplashScreen/SplashScreen.cs
@@ -7,7 +7,7 @@ using Newtonsoft.Json;
 using System.Collections;
 using Facebook.Unity;
 using Facebook.MiniJSON;
-//using Fabric.Twitter;
+using Fabric.Twitter;
 public class SplashScreen : MonoBehaviour {
 	enum LoginType{
 		FaceBook = 1,
@@ -372,45 +372,45 @@ public class SplashScreen : MonoBehaviour {
 	}
 
 	public void startLogin () {
-//		TwitterSession session = Twitter.Session;
-//		Twitter.LogIn (LoginComplete, LoginFailure);
+		TwitterSession session = Twitter.Session;
+		Twitter.LogIn (LoginComplete, LoginFailure);
 	}
 
-//	public void LoginComplete (TwitterSession session) {
-//		// Start composer or request email
-//		startRequestEmail();
-//		string username = session.userName;
-//		long id = session.id;
-//
-//	}
+	public void LoginComplete (TwitterSession session) {
+		// Start composer or request email
+		startRequestEmail();
+		string username = session.userName;
+		long id = session.id;
 
-//	public void LoginFailure (ApiError error) {
-//		//LoadingIndicatorManager.Hide();
-//		UnityEngine.Debug.Log ("code=" + error.code + " msg=" + error.message);
-//	}
-//
-//	public void TwitterLogout()
-//	{
-//		Twitter.LogOut ();
-//	}
-//
-//	public void startRequestEmail () {
-//		TwitterSession session = Twitter.Session;
-//		if (session != null) {
-//			Twitter.RequestEmail (session, requestEmailComplete, requestEmailFailure);
-//		} else {
-//			startLogin();
-//		}
-//	}
-//
-//	public void requestEmailComplete (string email) {
-//		// Save email
-//		string Email = email;
-//		UnityEngine.Debug.Log ("Twitter Email ---:"+Email);
-//	}
-//
-//	public void requestEmailFailure (ApiError error) {
-//		UnityEngine.Debug.Log ("code=" + error.code + " msg=" + error.message);
-//	}
+	}
+
+	public void LoginFailure (ApiError error) {
+		//LoadingIndicatorManager.Hide();
+		UnityEngine.Debug.Log ("code=" + error.code + " msg=" + error.message);
+	}
+
+	public void TwitterLogout()
+	{
+		Twitter.LogOut ();
+	}
+
+	public void startRequestEmail () {
+		TwitterSession session = Twitter.Session;
+		if (session != null) {
+			Twitter.RequestEmail (session, requestEmailComplete, requestEmailFailure);
+		} else {
+			startLogin();
+		}
+	}
+
+	public void requestEmailComplete (string email) {
+		// Save email
+		string Email = email;
+		UnityEngine.Debug.Log ("Twitter Email ---:"+Email);
+	}
+
+	public void requestEmailFailure (ApiError error) {
+		UnityEngine.Debug.Log ("code=" + error.code + " msg=" + error.message);
+	}
 
 }
